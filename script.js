73d9083754ac1b18cb41c307abf8a693e7d1a4c2
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2023-06-15 15:41:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-function letterCombinations(input_digit) {
+function letterCombinations(digits) {
   //Complete the function
+	let arr = [];
+    let val = ["","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"];
+    let fun = function helper(dig,arr,cur,val) {
+        if(dig.length == 0) {
+            arr.push(cur);
+            return;
+        }
+
+        for(let i = 0;i <= 9; i++) {
+            if(i == (dig.charAt(0)-'0')) {
+                for(let j=0;j<val[i].length;j++) {
+                    let temp = cur + val[i].charAt(j);
+                    helper(dig.substring(1),arr,temp,val);
+                }
+            }
+        }
+    }
+
+    fun(digits,arr,"",val);
+
+    return arr[0] == "" ? [] : arr;
 }
 
 module.exports = letterCombinations;
